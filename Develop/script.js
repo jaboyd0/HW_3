a5737b300101d68ec9c0d5c2590dfab12fb98685
Joshua Boyd <jaboyd0@gmail.com>
2020-02-23 16:35:30 -0500

    created stipulations requiring password length and at least one caracter selection

--- Develop/script.js
+++ Develop/script.js
@@ -1,48 +1,63 @@
-// Assignment Code
+// Initial Variables
 var generateBtn = document.querySelector("#generate");
-var passwordLength = prompt("Enter number of characters needed for password: \n(suggested 8-128 for security)")
-var lowerCase =confirm("Do you want lower case characters included?")
-var upperCase =confirm("Do you want upper case characters included?")
-var numeric =confirm("Do you want numeric characters included?")
-var special =confirm("Do you want special characters included?")
 var passwordText = document.querySelector("#password");
 
-if (lowerCase) {
-  var lowerArray = "abcdefghijklmnopqrstuvwxyz".split("");
-} else {
-  var lowerArray = ""
-}
 
-if (upperCase) {
-  var upperArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-} else {
-  var upperArray = ""
-}
 
-if (numeric) {
-  var numericArray = "0123456789".split("");
-} else {
-  var numericArray = ""
-}
-
-if (special) {
-  var specialArray = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("");
-} else {
-  var specialArray = ""
-}
-
-var characterPool = lowerArray.concat(upperArray, numericArray, specialArray);
-
-console.log (lowerArray)
-console.log (upperArray)
-console.log (numericArray)
-console.log (specialArray)
-console.log (characterPool)
-
-// Write password to the #password input
+// function to collect desired password info
 function writePassword() {
   passwordText.value = "";
-  // var password = generatePassword();
+
+  var passwordLength = prompt("Enter number of characters needed for password: \n(must be between 8-128 characters for security)") 
+    if (parseInt(passwordLength) < 8) {
+      alert ("Minimum Password length is 8")
+      return;
+    }
+    if (parseInt(passwordLength) > 128) {
+      alert ("Maximum password length is 128")
+      return;
+    }
+      
+  var lowerCase =confirm("Do you want lower case characters included?")
+  var upperCase =confirm("Do you want upper case characters included?")
+  var numeric =confirm("Do you want numeric characters included?")
+  var special =confirm("Do you want special characters included?")
+
+  if (lowerCase) {
+    var lowerArray = "abcdefghijklmnopqrstuvwxyz".split("");
+  } else {
+    var lowerArray = ""
+  }
+  
+  if (upperCase) {
+    var upperArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  } else {
+    var upperArray = ""
+  }
+  
+  if (numeric) {
+    var numericArray = "0123456789".split("");
+  } else {
+    var numericArray = ""
+  }
+  
+  if (special) {
+    var specialArray = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("");
+  } else {
+    var specialArray = ""
+  }
+  
+if (lowerCase !== true && upperCase !== true && numeric !== true && special !== true) {
+  alert ("Please select at least one character type")
+  return;
+}
+
+  var characterPool = lowerArray.concat(upperArray, numericArray, specialArray);
+
+  console.log (characterPool)
+
+  // random character generator
+
   for(var i = 0; i < parseInt(passwordLength); i++) {
   var password = characterPool[Math.floor(Math.random() * characterPool.length)]
 
@@ -50,6 +65,6 @@ function writePassword() {
   }
 }
 
-// Add event listener to generate button
+
 
 generateBtn.addEventListener("click", writePassword);
