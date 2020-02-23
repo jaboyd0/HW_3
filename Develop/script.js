323f37398314f9fcbf0c5ba9d452f3f4a2ebd3a1
Joshua Boyd <jaboyd0@gmail.com>
2020-02-22 20:05:15 -0500

    random character fuction finished

--- Develop/script.js
+++ Develop/script.js
@@ -4,15 +4,50 @@ var passwordLength = prompt("Enter number of characters needed for password: \n(
 var lowerCase =confirm("Do you want lower case characters included?")
 var upperCase =confirm("Do you want upper case characters included?")
 var numeric =confirm("Do you want numeric characters included?")
-var Special =confirm("Do you want special characters included?")
+var special =confirm("Do you want special characters included?")
+var characterPool
+
+if (lowerCase) {
+  var lowerArray = "abcdefghijklmnopqrstuvwxyz".split("");
+} else {
+  var lowerArray = ""
+}
+
+if (upperCase) {
+  var upperArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+} else {
+  var upperArray = ""
+}
+
+if (numeric) {
+  var numericArray = "0123456789".split("");
+} else {
+  var numericArray = ""
+}
+
+if (special) {
+  var specialArray = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("");
+} else {
+  var specialArray = ""
+}
+
+var characterPool = lowerArray.concat(upperArray, numericArray, specialArray);
+
+console.log (lowerArray)
+console.log (upperArray)
+console.log (numericArray)
+console.log (specialArray)
+console.log (characterPool)
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
+  for(var i = 0; i < parseInt(passwordLength); i++) {
+  var password = characterPool[Math.floor(Math.random() * characterPool.length)]
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  }
 }
 
 // Add event listener to generate button
